a31c183bf48c9e1bc08c0270e4b563a2adc506cc
abdieltg <abdieltorog@gmail.com>
2026-03-25 13:40:21 -0300

    Add files via upload

---
+++ Java_Login.js
@@ -0,0 +1,22 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let usuarioGuardado = JSON.parse(localStorage.getItem("usuario"));
+
+  let usuario = document.getElementById("loginUsuario").value;
+  let password = document.getElementById("loginPassword").value;
+
+  let mensaje = document.getElementById("mensajeLogin");
+
+  if (
+    usuarioGuardado &&
+    usuario === usuarioGuardado.usuario &&
+    password === usuarioGuardado.password
+  ) {
+    mensaje.textContent = "Inicio de sesión exitoso";
+    mensaje.className = "mensaje exito";
+  } else {
+    mensaje.textContent = "Usuario o contraseña incorrectos";
+    mensaje.className = "mensaje error";
+  }
+});
